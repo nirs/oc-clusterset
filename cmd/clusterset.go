ad41c11e770006491ac1bb1a46fc716ad4818cc9
Nir Soffer <nsoffer@redhat.com>
2023-09-10 01:49:35 +0300

    Validate clusterset config when reading
    It is better to catch users configuration errors early so we don't add
    junk to the default ~/.kube/config.
    
    - reject empty clusters
    - reject invalid current-context
    - missing or empty keys in cluster

--- cmd/clusterset.go
+++ cmd/clusterset.go
@@ -4,8 +4,10 @@
 package cmd
 
 import (
+	"fmt"
 	"log"
 	"os"
+	"strings"
 
 	"sigs.k8s.io/yaml"
 )
@@ -36,5 +38,59 @@ func loadClusterset(filename string) (*Clusterset, error) {
 		return nil, err
 	}
 
+	if err := clusterset.Validate(); err != nil {
+		return nil, err
+	}
+
 	return &clusterset, nil
+}
+
+func (c *Clusterset) Validate() error {
+	if len(c.Clusters) == 0 {
+		return fmt.Errorf("invalid config: no clusters")
+	}
+
+	if c.CurrentContext != "" {
+		for _, cluster := range c.Clusters {
+			if cluster.Name == c.CurrentContext {
+				return nil
+			}
+		}
+
+		return fmt.Errorf(
+			"invalid config: invalid %q: no cluster named %q",
+			"current-context", c.CurrentContext,
+		)
+	}
+
+	for _, cluster := range c.Clusters {
+		if err := cluster.Validate(); err != nil {
+			return fmt.Errorf("invalid config: %s", err)
+		}
+	}
+
+	return nil
+}
+
+func (c *Cluster) Validate() error {
+	var missing []string
+
+	if c.Name == "" {
+		missing = append(missing, "name")
+	}
+	if c.Url == "" {
+		missing = append(missing, "url")
+	}
+	if c.Username == "" {
+		missing = append(missing, "username")
+	}
+	if c.Password == "" {
+		missing = append(missing, "password")
+	}
+
+	if len(missing) > 0 {
+		return fmt.Errorf("missing required keys: %s", strings.Join(missing, ", "))
+	}
+
+	return nil
 }
